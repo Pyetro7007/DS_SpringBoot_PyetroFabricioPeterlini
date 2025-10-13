9bfca382a2cad7859039ceabef915c94bb94e351
Pyetro7007 <pyetrofabricio2014@gmail.com>
2025-10-13 10:03:08 -0300

    Estrutura

---
+++ api-teste/src/test/java/br/gov/sp/etec/bentao/api_teste/ApiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.etec.bentao.api_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
